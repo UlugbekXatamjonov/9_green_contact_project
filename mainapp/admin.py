d979c91e91634252371eef8c1e2d57448a8fc414
UlugbekXatamjonov <xatamjonovulugbek17@gmail.com>
2025-12-16 15:57:41 +0500

    16-12-25

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -6,6 +6,8 @@ from .models import Check_in
 
 @admin.register(Check_in)
 class Check_in_Admin(admin.ModelAdmin):
-    list_display  = ("full_name", "school")
+    list_display  = ("full_name", "school", "grade", "created_on")
+    list_filter = ('school', 'grade', "created_on")
+    search_fields = ("full_name", 'school', "phone_number")
 
 
